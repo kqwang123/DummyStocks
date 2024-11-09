aa4a314d2a9d34c7cd47279cc77217b1920e2988
kqwang123 <kqwang123@gmail.com>
2024-11-09 16:05:54 -0600

    WORKING FETCH ARTICLES

--- back-end/app.py
+++ back-end/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request
 import requests
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Your News API key (replace with your actual API key)
 API_KEY = '71d7039eaa7447e281f44fc298daf45b'
@@ -9,14 +11,14 @@ API_KEY = '71d7039eaa7447e281f44fc298daf45b'
 # News API endpoint
 NEWS_API_URL = 'https://newsapi.org/v2/everything'
 
-@app.route('/apple', methods=['GET'])
+@app.route('/search', methods=['GET'])
 def home():
     # Get the keyword from the form
-    keyword = 'apple'
+    keywords = request.args.get('keywords')
     
     # Make a request to the News API with the specified keyword
     params = {
-        'q': keyword,
+        'q': keywords,
         'apiKey': API_KEY,
         'pageSize': 10  # Limit results to 10 articles
     }
@@ -28,7 +30,7 @@ def home():
     else:
         articles = []
         error_message = "Error fetching news articles"
-    
+            
     return articles
     
     return []
