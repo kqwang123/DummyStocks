896199e91f1884725387505375b5f3168ad60704
kqwang123 <kqwang123@gmail.com>
2024-11-10 00:24:44 -0600

    Added procfile

--- back-end/app.py
+++ back-end/app.py
@@ -10,60 +10,64 @@ import finnhub
 from dotenv import load_dotenv
 import os
 
-app = Flask(__name__)
-CORS(app)
-
-# News API endpoint
-NEWS_API_URL = 'https://newsapi.org/v2/everything'
-load_dotenv()
-
-FINNHUB_API_KEY = os.getenv('FINNHUB_API_KEY')
-NEWS_API_API_KEY = os.getenv('NEWS_API_API_KEY')
-finnhub_client = finnhub.Client(api_key=FINNHUB_API_KEY)
-
-@app.route('/search', methods=['GET'])
-def search():
-    # Get the keyword from the form
-    keywords = request.args.get('keywords')
+def create_app():
     
-    # Make a request to the News API with the specified keyword
-    params = {
-        'q': keywords,
-        'apiKey': NEWS_API_API_KEY,
-        'pageSize': 10  # Limit results to 10 articles
-    }
-    response = requests.get(NEWS_API_URL, params=params)
-    
-    # Check if the response was successful
-    if response.status_code == 200:
-        articles = response.json()['articles']
-    else:
-        articles = []
-        error_message = "Error fetching news articles"
-        print(error_message)
-            
-    return articles
+    app = Flask(__name__)
+    CORS(app)
 
-@app.route('/scrape', methods=['GET'])
-def scrape():
-    url = request.args.get('url')
-    
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
+    # News API endpoint
+    NEWS_API_URL = 'https://newsapi.org/v2/everything'
+    load_dotenv()
+
+    FINNHUB_API_KEY = os.getenv('FINNHUB_API_KEY')
+    NEWS_API_API_KEY = os.getenv('NEWS_API_API_KEY')
+    finnhub_client = finnhub.Client(api_key=FINNHUB_API_KEY)
+
+    @app.route('/search', methods=['GET'])
+    def search():
+        # Get the keyword from the form
+        keywords = request.args.get('keywords')
         
-        article = soup.find('article')  
-        if article:
-            return article.get_text(strip=True)  
+        # Make a request to the News API with the specified keyword
+        params = {
+            'q': keywords,
+            'apiKey': NEWS_API_API_KEY,
+            'pageSize': 10  # Limit results to 10 articles
+        }
+        response = requests.get(NEWS_API_URL, params=params)
+        
+        # Check if the response was successful
+        if response.status_code == 200:
+            articles = response.json()['articles']
         else:
-            return "Article content not found."
-    else:
-        return f"Failed to retrieve URL: {response.status_code}"
-    
-@app.route('/get_stock', methods=['GET'])
-def get_stock():
+            articles = []
+            error_message = "Error fetching news articles"
+            print(error_message)
+                
+        return articles
 
-    print(finnhub_client.earnings_calendar(_from="2021-06-10", to="2021-06-30", symbol="", international=False))
+    @app.route('/scrape', methods=['GET'])
+    def scrape():
+        url = request.args.get('url')
+        
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            
+            article = soup.find('article')  
+            if article:
+                return article.get_text(strip=True)  
+            else:
+                return "Article content not found."
+        else:
+            return f"Failed to retrieve URL: {response.status_code}"
+        
+    @app.route('/get_stock', methods=['GET'])
+    def get_stock():
 
-if __name__ == '__main__':
-    app.run(debug=False)
+        print(finnhub_client.earnings_calendar(_from="2021-06-10", to="2021-06-30", symbol="", international=False))
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+    return app
---
+++ back-end/run.py
@@ -0,0 +1,9 @@
+from app import create_app
+
+app = create_app()
+
+# Uncomment the following to run the app locally, but comment it out before pushing to GitHub
+# DO NOT EDIT FILE OTHERWISE
+
+# if __name__ == '__main__':
+#     app.run(port='5555', debug=True)
